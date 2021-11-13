d0e3582592da2566c5abb4e3a6415d7d888e4fc9
SANDESH SOBARAD <57451618+SANDESHSOBARAD@users.noreply.github.com>
2021-11-13 00:01:08 -0800

    movement of paddle

--- pong.py
+++ pong.py
@@ -6,7 +6,7 @@ wn.bgcolor("black")
 wn.setup(width = 800, height = 600)
 wn.tracer(0)
 
-# Paddle A
+# Paddle_A
 
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
@@ -26,5 +26,44 @@ paddle_b.shapesize(stretch_wid=(5), stretch_len=(1))
 paddle_b.penup()
 paddle_b.goto(350,0)
 
+#ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0,0)
+
+# Function to move paddle_A
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+    
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+    
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+
+#keyborad keys to call the function
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+
+    
+    
 while True:
     wn.update()
