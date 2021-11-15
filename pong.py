4f623e28f976641d16317f5aab6c6493fc57ff39
SANDESH SOBARAD <57451618+SANDESHSOBARAD@users.noreply.github.com>
2021-11-14 23:04:52 -0800

    set the ball's movements

--- pong.py
+++ pong.py
@@ -27,14 +27,27 @@ paddle_b.penup()
 paddle_b.goto(350,0)
 
 #ball
+
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0,0)
+ball.dx = 2
+ball.dy = -2
+
+#pen // justlike the turtle
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("courier", 24, "normal"))
 
 # Function to move paddle_A
+
 def paddle_a_up():
     y = paddle_a.ycor()
     y += 20
@@ -62,8 +75,37 @@ wn.onkeypress(paddle_a_up, "w")
 wn.onkeypress(paddle_a_down, "s")
 wn.onkeypress(paddle_b_up, "Up")
 wn.onkeypress(paddle_b_down, "Down")
-
-    
     
 while True:
     wn.update()
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+    
+    #border check
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+        
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+        
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        
+    #paddle and ball collisions
+    
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
+        ball.setx(340)
+        ball.dx *= -1
+        
+    if(ball.xcor() < -340 and ball.xcor() > -350)and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
+        ball.setx(340)
+        ball.dx *= -1
+        
+    
