cd0258401bb355192426ded324dcbdd636bada34
SANDESH SOBARAD <57451618+SANDESHSOBARAD@users.noreply.github.com>
2021-11-16 22:05:29 -0800

    ready to play!

--- pong.py
+++ pong.py
@@ -1,111 +1,111 @@
 import turtle
- 
+
 wn = turtle.Screen()
-wn.title("Pong by sandesh")
-wn.bgcolor("black")
-wn.setup(width = 800, height = 600)
+wn.title('Pong')
+wn.bgcolor('black')
+wn.setup(width=800, height=600)
 wn.tracer(0)
 
-# Paddle_A
-
+# Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
-paddle_a.shape("square")
-paddle_a.color("white")
-paddle_a.shapesize(stretch_wid=(5), stretch_len=(1))
+paddle_a.shape('square')
+paddle_a.color('white')
 paddle_a.penup()
 paddle_a.goto(-350, 0)
+paddle_a.shapesize(5, 1)
 
-#paddle_B
-
+# Paddle B
 paddle_b = turtle.Turtle()
 paddle_b.speed(0)
-paddle_b.shape("square")
-paddle_b.color("white")
-paddle_b.shapesize(stretch_wid=(5), stretch_len=(1))
+paddle_b.shape('square')
+paddle_b.color('white')
 paddle_b.penup()
-paddle_b.goto(350,0)
+paddle_b.goto(350, 0)
+paddle_b.shapesize(5, 1)
 
-#ball
-
+# Ball
 ball = turtle.Turtle()
 ball.speed(0)
-ball.shape("square")
-ball.color("white")
+ball.shape('square')
+ball.color('white')
 ball.penup()
-ball.goto(0,0)
-ball.dx = 2
-ball.dy = -2
+ball.dx = 0.50
+ball.dy = 0.50
 
-#pen // justlike the turtle
+# Pen
 pen = turtle.Turtle()
 pen.speed(0)
-pen.color("white")
+pen.color('white')
 pen.penup()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align='center', font=('Courier', 24, 'bold'))
 pen.hideturtle()
-pen.goto(0,260)
-pen.write("Player A: 0  Player B: 0", align="center", font=("courier", 24, "normal"))
 
-# Function to move paddle_A
+# Score
+score_a = 0
+score_b = 0
 
 def paddle_a_up():
     y = paddle_a.ycor()
     y += 20
     paddle_a.sety(y)
 
-def paddle_a_down():
-    y = paddle_a.ycor()
-    y -= 20
-    paddle_a.sety(y)
-    
 def paddle_b_up():
     y = paddle_b.ycor()
     y += 20
     paddle_b.sety(y)
-    
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y += -20
+    paddle_a.sety(y)
+
 def paddle_b_down():
     y = paddle_b.ycor()
-    y -= 20
+    y += -20
     paddle_b.sety(y)
 
-
-#keyborad keys to call the function
+# Keyboard binding
 wn.listen()
-wn.onkeypress(paddle_a_up, "w")
-wn.onkeypress(paddle_a_down, "s")
-wn.onkeypress(paddle_b_up, "Up")
-wn.onkeypress(paddle_b_down, "Down")
-    
+wn.onkeypress(paddle_a_up, 'w')
+wn.onkeypress(paddle_a_down, 's')
+wn.onkeypress(paddle_b_up, 'Up')
+wn.onkeypress(paddle_b_down, 'Down')
+
+
+# Main game loop
 while True:
     wn.update()
+
+    # Moving Ball
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
-    
-    #border check
-    if ball.ycor() > 290:
-        ball.sety(290)
+
+    # Border checking
+    if ball.ycor() > 290 or ball.ycor() < -290:
         ball.dy *= -1
-        
-    if ball.ycor() < -290:
-        ball.sety(-290)
-        ball.dy *= -1
-        
+
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
-        
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align='center', font=('Courier', 24, 'bold'))
+
     if ball.xcor() < -390:
         ball.goto(0, 0)
+
         ball.dx *= -1
-        
-    #paddle and ball collisions
-    
-    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align='center', font=('Courier', 24, 'bold'))
+
+    # Paddle and ball collisions
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 60 and ball.ycor() > paddle_b.ycor() -60):
         ball.setx(340)
         ball.dx *= -1
-        
-    if(ball.xcor() < -340 and ball.xcor() > -350)and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
-        ball.setx(340)
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 60 and ball.ycor() > paddle_a.ycor() -60):
+        ball.setx(-340)
         ball.dx *= -1
-        
-    
